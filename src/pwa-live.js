d50b58997580549e06ea920f359d63ceae134b4c
Israel Duran <israel_duran@outlook.com>
2019-07-19 19:59:10 -0500

    Se ha agregado el ruteo con dile-pages y dile-tabs
    Se han agregado los componentes dile-pages y dile-tabs para el ruteo de la aplicación,
    se cambiaron los links por tabes

--- src/pwa-live.js
+++ src/pwa-live.js
@@ -2,21 +2,20 @@ import { LitElement, html, css } from 'lit-element';
 import './views/view-about';
 import './views/view-home';
 import './views/view-contact';
+import 'dile-tabs/dile-tabs';
+import 'dile-pages/dile-pages';
 class PwaLive extends LitElement {
 	static get styles(){
 		return css`
 			:host{
 				display:block;
 				padding: 15px;
+				--dile-tab-background-color: transparent;
+				--dile-tab-selected-background-color: transparent;
+				--dile-tab-selected-text-color: #1020c0;
 			}
 			h1{
 				font-weight: 300px;
-			}
-			.page{
-				display: none;
-			}
-			.page[active]{
-				display: block;
 			}
 		`;
 	}
@@ -33,17 +32,29 @@ class PwaLive extends LitElement {
 	render() {
 		return html`
 			<h1>my app</h1>
+			<!--
 			<nav>
+
 				<a href="#" @click="${this.go}" name="home">Home</a> |
 				<a href="#" @click="${this.go}" name="about">About</a> |
 				<a href="#" @click="${this.go}" name="contact">Contact</a>
+			
 			</nav>
-			<main>
-			<view-home class="page" ?active=${this.selected == 'home'}></view-home>
-			<view-about class="page" ?active=${this.selected =='about'}></view-about>
-			<view-contact class="page" ?active=${this.selected =='contact'}></view-contact>
-			</main>
+			-->
+			<dile-tabs selected="${this.selected}" attrForSelected="name" @dile-tabs-selected-changed="${this.selectedChanged}">
+				<dile-tab name="home">Home</dile-tab>
+				<dile-tab name="about">About</dile-tab>
+				<dile-tab name="contact">Contact</dile-tab>
+			</dile-tabs>
+			<dile-pages selected="${this.selected}" attrForSelected="name">
+				<view-home name="home" ?active=${this.selected == 'home'}></view-home>
+				<view-about name="about" ?active=${this.selected =='about'}></view-about>
+				<view-contact name="contact" ?active=${this.selected =='contact'}></view-contact>
+			</dile-pages>
 		`;
+	}
+	selectedChanged(e){
+		this.selected = e.detail;
 	}
 	go(e){
 		e.preventDefault();
